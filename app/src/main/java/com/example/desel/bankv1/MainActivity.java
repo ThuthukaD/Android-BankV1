280d1bbcd7cf4af85415fe49871cc8fe063cd9ac
Thuthuka <deselet@gmail.com>
2019-05-16 10:33:27 +0200

    UI Improvements and new layouts added

--- app/src/main/java/com/example/desel/bankv1/MainActivity.java
+++ app/src/main/java/com/example/desel/bankv1/MainActivity.java
@@ -4,13 +4,29 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+    // VARIABLES
+
+    // Floating Buttons
+    FloatingActionButton fbtnPay;
+    FloatingActionButton fbtnTransfer;
+    FloatingActionButton fbtnBuy;
+    FloatingActionButton fbtnBudget;
+    FloatingActionButton fbtnTBD;
+
+    // Card Views
+    CardView cvCard1;
+    CardView cvCard2;
+    CardView cvCard3;
+    CardView cvCard4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,24 +35,95 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // INITIALISING
+
+        // Floating Buttons
+        fbtnPay = findViewById(R.id.fbtnPay);
+        fbtnTransfer = findViewById(R.id.fbtnTransfer);
+        fbtnBuy = findViewById(R.id.fbtnBuy);
+        fbtnBudget = findViewById(R.id.fbtnBudget);
+        fbtnTBD = findViewById(R.id.fbtnTBD);
+
+        // Card Views
+        cvCard1 = findViewById(R.id.cvCard1);
+        cvCard2 = findViewById(R.id.cvCard2);
+        cvCard3 = findViewById(R.id.cvCard3);
+        cvCard4 = findViewById(R.id.cvCard4);
+
+        floatButtons();
+    }
+
+    private void floatButtons()
+    {
+        fbtnPay.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+        fbtnTransfer.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+        fbtnBuy.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+        fbtnBudget.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                
+            }
+        });
+
+        // Example button for upcoming features
+        fbtnTBD.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText
+                        (MainActivity.this, "Locked, Upcoming Feature",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
